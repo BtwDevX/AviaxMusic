e9bfb7ab764a57ed02c1af357477994daf798ee5
Dev meena <btwdevxd@gmail.com>
2024-08-18 22:48:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/BtwDevX/a",
+    "https://github.com/BtwDevX/b",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
